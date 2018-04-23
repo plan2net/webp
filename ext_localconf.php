221cc54f42a0bfc236120af86f26ff736ef87e65
Wolfgang Klinger <wk@plan2.net>
2018-04-23 15:18:23 +0200

    Fix instanciating $signalSlotDispatcher

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,7 @@
 
 defined('TYPO3_MODE') or die('Access denied');
 
+$signalSlotDispatcher = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
 $signalSlotDispatcher->connect(
     \TYPO3\CMS\Core\Resource\ResourceStorage::class,
     \TYPO3\CMS\Core\Resource\Service\FileProcessingService::SIGNAL_PostFileProcess,
