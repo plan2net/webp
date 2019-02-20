1bb343680c4ca80c770020e2a2af324f14cfea9a
Andreas Kießling <andreas@powered-by-coffee.de>
2019-02-20 16:13:47 +0100

    return single extension configuration value

--- Classes/Processing/Webp.php
+++ Classes/Processing/Webp.php
@@ -146,7 +146,7 @@ class Webp
         }
 
         if ($key !== null && isset($configuration[$key])) {
-            return (string)$key;
+            return (string)$configuration[$key];
         }
 
         return $configuration;
