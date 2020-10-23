18ffee84befb41f98a8f8e1afdad27fe390b0971
Ingo Fabbri <inf@mmc-agentur.at>
2020-10-23 14:21:04 +0200

    [TASK] add index for tx_webp_failed

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,8 +1,10 @@
 # This is an internal table, no TCA
-CREATE TABLE tx_webp_failed (
-	uid int(11) NOT NULL auto_increment,
-    file_id INT(11) NOT NULL DEFAULT '0',
-    configuration TEXT,
+CREATE TABLE tx_webp_failed
+(
+    uid                int(11) NOT NULL auto_increment,
+    file_id            INT(11) NOT NULL DEFAULT '0',
+    configuration      TEXT,
     configuration_hash VARCHAR(40),
-	PRIMARY KEY (uid)
+    PRIMARY KEY (uid),
+    KEY configuration (file_id, configuration_hash)
 );
